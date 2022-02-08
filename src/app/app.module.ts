69119b5782cbc5885b3f55368323dc590079db4c
gostaar <bauwens.delphine@gmail.com>
2022-02-08 19:15:11 +0100

    products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductsModule } from './products/products.module'
 
 @NgModule({
   declarations: [
